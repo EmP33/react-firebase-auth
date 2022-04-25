672cf2f7acddbac6198ecf912cc2d2881615e139
EmP33 <purwinmarek16@gmail.com>
2022-04-25 21:00:41 +0200

    Add basic login/logout/register structure

--- src/firebase.js
+++ src/firebase.js
@@ -1,14 +1,15 @@
-import firebase from "firebase";
-import "firebase/auth";
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
 
-const app = firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
-  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.REACT_APP_APP_ID,
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyCj4e29XkigwiL6x3wOcW1C5NqLfAvTr-I",
+  authDomain: "auth-demo-dev-af132.firebaseapp.com",
+  projectId: "auth-demo-dev-af132",
+  storageBucket: "auth-demo-dev-af132.appspot.com",
+  messagingSenderId: "1025272889081",
+  appId: "1:1025272889081:web:4ee0d331a66c9ca369ec28",
+};
 
-export const auth = app.auth();
-export default app;
+const app = initializeApp(firebaseConfig);
+
+export const auth = getAuth(app);
